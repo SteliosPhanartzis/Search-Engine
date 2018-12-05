13711d57b458dd7da636ef1e5a2ff2839e2559c1
SteliosPhanartzis <stelios.phan@gmail.com>
2018-12-05 17:44:46 -0500

    Updated port

--- routes/app.js
+++ routes/app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.all('/*', function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', 'Content-Type,accept,access_token,X-Requested-With');
+next();
+});
+
 app.set('views', path.join(__dirname, '../','views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
