9e5f080388ac27b75b6e1f4cffed7586c89d2fb0
SteliosPhanartzis <stelios.phan@gmail.com>
2018-12-05 10:31:49 -0500

    Updated Files

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const app = new express();
 var path = require("path");
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, '../','views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(request, response){
-    response.sendFile(path.join(__dirname + '/views/index.html'));
+    response.sendFile(path.join(__dirname, '../', '/views/index.html'));
 });
 
 app.get('/search', function(request, response){
-	//test data
-	// var results = "<table>" + 
-	// "<tr><td>Henlo</td></tr>" +
- //    "<tr><td>A good result of a search description is this and boi howdy does it do a thing</td></tr>" +
- //    "<tr><td>http://www.fakeurl.com/thiswillstealyourcreditcardinfo?givemeyourmoney</td></tr>" +
- //    "<tr><td>Oh Waow</td></tr>" + 
- //    "<tr><td>I can't believe you found another result. Thas insane in the membrane. Henlo.</td></tr>"+
- //    "<tr><td>http://www.falseurl.com/thiswillstillstealyourcreditcardinfo?pleasegivemeyourmoney</td></tr>" +
- //  "</table>";
  console.log(request);
  var results = request;
-    response.render(path.join(__dirname + '/views/search.html'),{results:results});
+    response.render('search.html'),{results:results}
     // response.send('hello world');
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3300);
 
 
 
-console.log("Running at Port 8080");
+console.log("Running at Port 3300");
--- routes/app.js
+++ routes/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = new express();
 var path = require("path");
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, '../','views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.get('/', function(request, response){
-    response.sendFile(path.join(__dirname + '/views/index.html'));
+    response.sendFile(path.join(__dirname, '../', '/views/index.html'));
 });
 
 app.get('/search', function(request, response){
-    // response.sendFile(path.join(__dirname + '/views/search.html'));
-    response.send('hello world');
+ console.log(request.query.searchid);
+ var results = request.query.searchid;
+    response.render(path.join(__dirname, '../', 'views/search.html')),{results:results}
+    // response.send('hello world');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3300);
 
 
 
-console.log("Running at Port 3000");
+console.log("Running at Port 3300");
