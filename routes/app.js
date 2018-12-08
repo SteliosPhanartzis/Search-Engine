3f456bb5bd699ba07a9084f530f4d7dda747a306
SteliosPhanartzis <steliftw@gmail.com>
2018-12-08 03:31:16 -0500

    Fixed MIME error, commented out SQL connection

--- routes/app.js
+++ routes/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = new express();
 const path = require("path");
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+//const mysql = require('mysql');
 const PORT = process.env.PORT || 5043;
-const sqlconn = mysql.createConnection({
-  host     : 'h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user     : 'ssam7xjm54kms0e0',
-  password : 'tr9diqtobus8nu5y',
-  database : 'hei5xkowlg9oo6t4'
-});
+// const sqlconn = mysql.createConnection({
+//   host     : 'h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//   user     : 'ssam7xjm54kms0e0',
+//   password : 'tr9diqtobus8nu5y',
+//   database : 'hei5xkowlg9oo6t4'
+// });
 
-sqlconn.connect();
+//sqlconn.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //sqlconn.connect()
@@ -30,7 +30,7 @@ res.header('Access-Control-Allow-Headers', 'Content-Type,accept,access_token,X-R
 next();
 });
 
-
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, '../','views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -45,20 +45,20 @@ app.get('/search', function(request, response){
  	//console.log(request.query.sid);
  	var results = request.query.sid;
  	//construct and pass query to mysql
- 	var qry = "SELECT urlNames FROM URL WHERE urlNames LIKE CONCAT('%','" + results + "'',%')";
-	var out = sqlconn.query(qry, function (err, result, fields) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		var rows = JSON.parse(JSON.stringify(result[0]));
-		console.log("Result: " + rows);
-	});
+ 	//var qry = "SELECT urlNames FROM URL WHERE urlNames LIKE CONCAT('%','" + results + "'',%')";
+	//var out = sqlconn.query(qry, function (err, result, fields) {
+	//	if (err) {
+	//		console.log(err);
+	//		return;
+	//	}
+	//	var rows = JSON.parse(JSON.stringify(result[0]));
+	//	console.log("Result: " + rows);
+	//});
  	response.render(path.join(__dirname, '../', 'views/search.html'),{results:results})
 	//Connection Timeout
-	setTimeout(() => {
-		sqlconn.end();
-		}, 10000);
+	//setTimeout(() => {
+	//	sqlconn.end();
+	//	}, 10000);
 });
 
 app.listen(PORT,() => {
