0ef65f9e59e32d2fb53e541aefce988cf078037c
Maxwell <maxperryg@gmail.com>
2018-12-11 12:08:38 -0500

    try to figue out the insert query

--- Crawler/crawler.js
+++ Crawler/crawler.js
@@ -1,16 +1,26 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
+var mysql = require('mysql');
 
-var START_URL = "https://www.yahoo.com/news/";
+var START_URL = "https://mashable.com/";
 var SEARCH_WORD = "trump";
-var MAX_PAGES_TO_VISIT = 10;
+var MAX_PAGES_TO_VISIT = 1;
 
 var pagesVisited = {};
 var numPagesVisited = 0;
 var pagesToVisit = [];
 var url = new URL(START_URL);
-var baseUrl = url.protocol + "//" + url.hostname;
+var baseUrl;
+
+var con = mysql.createConnection({
+  host: "h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ssam7xjm54kms0e0",
+  password: "tr9diqtobus8nu5y",
+    database : 'hei5xkowlg9oo6t4',
+    debug : 'false'
+});
+
 
 pagesToVisit.push(START_URL);
 crawl();
@@ -21,6 +31,7 @@ function crawl() {
     return;
   }
   var nextPage = pagesToVisit.pop();
+    debugger;
 //    console.log(nextPage);
   if (nextPage in pagesVisited) {
     // We've already visited this page, so repeat the crawl
@@ -30,6 +41,8 @@ function crawl() {
       return;
   }else {
     // New page we haven't visited
+    url = new URL(nextPage);
+    baseUrl = url.protocol + "//" + url.hostname;
     visitPage(nextPage, crawl);
   }
 }
@@ -50,27 +63,80 @@ function visitPage(url, callback) {
      }
      // Parse the document body
      var $ = cheerio.load(body);
-     var isWordFound = searchForWord($, SEARCH_WORD);
-     if(isWordFound) {
-       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
-     }
-       collectInternalLinks($);
+      var count = searchForWord($, SEARCH_WORD);
+      console.log(count);
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+          count.forEach(function(){
+              var sql = "INSERT INTO URL (terms, urls) VALUES ("+this+", "+url+")";
+          })
+        //var sql = "INSERT INTO URL (terms, urls) VALUES ("+, '65-30 Kissena Blvd.')";
+      });
+      //console.log(typeof(count));
+
+      collectInternalLinks($);
+//     if(isWordFound) {
+//       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+//     }
+       //collectInternalLinks($);
        // In this short program, our callback is just calling crawl()
        callback();
   });
 }
 
 function searchForWord($, word) {
-  var bodyText = $('html > body').text().toLowerCase();
-  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+    var h1="";
+    var h2="";
+    var h3="";
+    var p = "";
+    var bodyText = $("h1").each(function(){
+        h1+=$(this).text()+" ";
+    })
+    bodyText = $("h2").each(function(){
+        h2+=$(this).text()+" ";
+    })
+    bodyText = $("h3").each(function(){
+        h3+=$(this).text()+" ";
+    })
+    bodyText = $("p").each(function(){
+        p+=$(this).text()+" ";
+    })
+    console.log(p);
+    var meat = h1+h2+h3+p;
+    //console.log(countWords(meat));
+    return(countWords(meat));
+
+}
+
+function countWords(sentence) {
+  var index = [],
+      words = sentence
+              .replace(/[.,?!;()"'-]/g, " ")
+              .replace(/\s+/g, " ")
+              .toLowerCase()
+              .split(" ");
+
+    words.forEach(function (word) {
+        if (!(index.hasOwnProperty(word))) {
+            index.push(word);
+        }
+        //index[word]++;
+    });
+
+    return index;
 }
 
 function collectInternalLinks($) {
-    var relativeLinks = $("a[href^='http']");
-    //debugger;
-    //var relativeLinks = $('a');
+    var relativeLinks = $("a[href^='/']");
     console.log("Found " + relativeLinks.length + " relative links on page");
     relativeLinks.each(function() {
+        console.log(baseUrl + $(this).attr('href'));
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
+    });
+    var absoluteLinks = $("a[href^='http']");
+    console.log("Found " + absoluteLinks.length + " absolute links on page");
+    absoluteLinks.each(function() {
         if(!($(this).attr('href') in pagesVisited)){
             console.log($(this).attr('href'));
             pagesToVisit.push($(this).attr('href'));
